f37e5c6d844617e4ef00e85133edeb99ff4b51bb
andreasdan <andreas.petersen01@gmail.com>
2021-05-01 16:26:38 +0200

    dummy data

--- src/routes/controllers/index.js
+++ src/routes/controllers/index.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/login', (req, res) => {
     return res.render('pages/index');
+});
+
+router.get('/', (req, res) => {
+    return res.render('pages/register');
+});
+
+router.get('/test', (req, res) => {
+    return res.render('pages/attendance', {
+        students: [
+            { firstName: 'Hans', lastName: 'Hansen', email: 'hanshansen@stud.kea.dk', active: 1, key: '84hOl4k5b8' },
+            { firstName: 'Peter', lastName: 'Petersen', email: 'peterpetersen@stud.kea.dk', active: 1, key: 'XbxcK6cqGX' },
+            { firstName: 'Jens', lastName: 'Jensen', email: 'jensjensen@stud.kea.dk', active: 1, key: 'EDQUuuNV7o' },
+            { firstName: 'Mark', lastName: 'Marksen', email: 'markmarksen@stud.kea.dk', active: 1, key: 'n7CHFF8qn1' },
+            { firstName: 'Karl', lastName: 'Karlsen', email: 'karlkarlsen@stud.kea.dk', active: 1, key: '5GYwj0ac4H' }
+        ]
+    })
 });
 
 module.exports = router;
