301bf8a66acbae8bfac45507577e876112a8323c
andreasdan <andreas.petersen01@gmail.com>
2021-05-20 22:56:29 +0200

    completed index controller (register and login)

--- src/routes/controllers/index.js
+++ src/routes/controllers/index.js
@@ -1,23 +1,47 @@
 const router = require('express').Router();
+const axios = require('axios');
 
 router.get('/login', (req, res) => {
     return res.render('pages/index');
 });
 
-router.post('/login', (req, res) => {
-    //TODO authenticate
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+        res.redirect('/');
+    }
+
+    const response = await axios({
+        method: 'post',
+        url: process.env.BACKEND_SERVER + '/api/auth/authenticate',
+        data: {
+            email, password
+        }
+    });
+
+    req.session.token = response.headers['x-auth-token'];
 });
 
 router.get('/', (req, res) => {
     return res.render('pages/register');
 });
 
-router.post('/register', (req, res) => {
-    //TODO make register
+router.post('/register', async (req, res) => {
+    const { key } = req.body;
+
+    const response = await axios({
+        method: 'post',
+        url: process.env.BACKEND_SERVER + '/attendancekey/validate/' + key
+    });
+
+    if (response.status == 200) {
+        
+    }
 });
 
 router.get('/logout', (req, res) => {
-    // TODO: clear session
+    delete req.session.token;
     return res.redirect('/login');
 });
 
