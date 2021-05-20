44ff1869eb3cc367c9d13029bcba48cf08014df9
andreasdan <andreas.petersen01@gmail.com>
2021-05-20 15:01:25 +0200

    courses fetch example

--- src/routes/controllers/courses.js
+++ src/routes/controllers/courses.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
+const axios = require('axios');
 
-router.get('/courses/:id', (req, res) => {
+router.get('/courses/:id', async (req, res) => {
+    const response = await axios({
+        method: 'get',
+        url: process.env.BACKEND_SERVER + '/course?id=' + id,
+        headers: {
+            'x-auth-token': req.session.token
+        }
+    });
 
+    res.render('pages/course', response);
 });
 
 router.get('/courses/:id/attendance', (req, res) => {
-
+    
 });
 
 module.exports = router;
